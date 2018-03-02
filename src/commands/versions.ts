3d8069a71d2a89ea1eddf772be155adb49c468d1
jeremy.smartt@teradata.com <jeremy.smartt@teradata.com>
2018-03-02 15:30:31 -0800

    Adding colors to cli

--- src/commands/versions.ts
+++ src/commands/versions.ts
@@ -3,6 +3,7 @@
  * TERADATA CORPORATION CONFIDENTIAL AND TRADE SECRET
  */
 
+import { red, blue } from 'colors';
 import { iCommand } from './commands';
 
 export default class Versions implements iCommand {
@@ -42,27 +43,27 @@ export default class Versions implements iCommand {
                                                                                                       
 `;
   execute() {
-    console.log(this.logo);
+    console.log(red(this.logo));
     let localPackageJson: any = require(process.cwd() + '/package.json');
     let cliPackageJson: any = require(__dirname + '/../../package.json');
 
     if (cliPackageJson && cliPackageJson.version) {
-      console.log("Covalent CLI Version: " + cliPackageJson.version);
+      console.log(blue("Covalent CLI Version: " + cliPackageJson.version));
     }
     if (localPackageJson && localPackageJson.dependencies) {
       if (localPackageJson.dependencies['@covalent/core']) {
-        console.log("Covalent Version: " + localPackageJson.dependencies["@covalent/core"]);
+        console.log(blue("Covalent Version: " + localPackageJson.dependencies["@covalent/core"]));
       }
       if (localPackageJson.dependencies['@angular/core']) {
-        console.log("Angular Version: " + localPackageJson.dependencies["@angular/core"]);
+        console.log(blue("Angular Version: " + localPackageJson.dependencies["@angular/core"]));
       }
       if (localPackageJson.dependencies['@angular/material']) {
-        console.log("Angular Material Version: " + localPackageJson.dependencies["@angular/material"]);
+        console.log(blue("Angular Material Version: " + localPackageJson.dependencies["@angular/material"]));
       }
     }
     if (localPackageJson && localPackageJson.devDependencies) {
       if (localPackageJson.devDependencies['@angular/cli']) {
-        console.log("Angular CLI Version: " + localPackageJson.devDependencies["@angular/cli"]);
+        console.log(blue("Angular CLI Version: " + localPackageJson.devDependencies["@angular/cli"]));
       }
     }
   }
