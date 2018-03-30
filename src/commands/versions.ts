ac08f2c0fba2e27d959e977e4257b66efa193652
jeremy.smartt@teradata.com <jeremy.smartt@teradata.com>
2018-03-30 12:27:03 -0700

    Updating to use versions of covalent, angular, and material from package.json in node_modules instead of top level package.json dependency section

--- src/commands/versions.ts
+++ src/commands/versions.ts
@@ -49,18 +49,30 @@ export default class Versions implements iCommand {
     }
     if (localPackageJson && localPackageJson.dependencies) {
       if (localPackageJson.dependencies['@covalent/core']) {
-        Utils.log(emoji.heavy_check_mark + " Covalent Version: " + localPackageJson.dependencies["@covalent/core"], blue);
+        let covPackageJson: any = require(process.cwd() + '/node_modules/@covalent/core/package.json');
+        if (covPackageJson && covPackageJson.version) {
+          Utils.log(emoji.heavy_check_mark + " Covalent Version: " + covPackageJson.version, blue);
+        }
       }
       if (localPackageJson.dependencies['@angular/core']) {
-        Utils.log(emoji.heavy_check_mark + " Angular Version: " + localPackageJson.dependencies["@angular/core"], blue);
+        let angPackageJson: any = require(process.cwd() + '/node_modules/@angular/core/package.json');
+        if (angPackageJson && angPackageJson.version) {
+          Utils.log(emoji.heavy_check_mark + " Angular Version: " + angPackageJson.version, blue);
+        }
       }
       if (localPackageJson.dependencies['@angular/material']) {
-        Utils.log(emoji.heavy_check_mark + " Angular Material Version: " + localPackageJson.dependencies["@angular/material"], blue);
+        let matPackageJson: any = require(process.cwd() + '/node_modules/@angular/material/package.json');
+        if (matPackageJson && matPackageJson.version) {
+          Utils.log(emoji.heavy_check_mark + " Angular Material Version: " + matPackageJson.version, blue);
+        }
       }
     }
     if (localPackageJson && localPackageJson.devDependencies) {
       if (localPackageJson.devDependencies['@angular/cli']) {
-        Utils.log(emoji.heavy_check_mark + " Angular CLI Version: " + localPackageJson.devDependencies["@angular/cli"], blue);
+        let angCliPackageJson: any = require(process.cwd() + '/node_modules/@angular/cli/package.json');
+        if (angCliPackageJson && angCliPackageJson.version) {
+          Utils.log(emoji.heavy_check_mark + " Angular CLI Version: " + angCliPackageJson.version, blue);
+        }
       }
     }
     Utils.log('');
